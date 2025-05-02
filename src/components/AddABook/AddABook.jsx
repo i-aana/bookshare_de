36cf4ab1853521b749c74ea604f77e83a1e5824c
gargi-patel11 <gargipatel2211@gmail.com>
2025-05-03 01:38:02 +0530

    added add a book feature

--- src/components/AddABook/AddABook.jsx
+++ src/components/AddABook/AddABook.jsx
@@ -1,4 +1,104 @@
 import React, { useState } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Book1 from "../../assets/arrival/profeth.jpg";
+import Book2 from "../../assets/arrival/algo.jpg";
+import Book3 from "../../assets/arrival/python.jpeg";
+import Book4 from "../../assets/arrival/softeng.jpeg";
+import Book5 from "../../assets/books/sihbook4.jpg";
+import { FaStar, FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
+const booksData = [
+  {
+    id: 1,
+    img: Book1,
+    title: "Professional Ethics",
+    rating: 5.0,
+    author: "V.S.Bagad,I.A. Dhotre and Uma Joshi",
+    price: 300,
+  },
+  {
+    id: 2,
+    img: Book2,
+    title: "Analysis and Design of Algorithms",
+    rating: 4.5,
+    author: "A.A. Puntambekar",
+    price: 230,
+  },
+  {
+    id: 3,
+    img: Book3,
+    title: "Python for Datascience",
+    rating: 4.7,
+    author: "I.A. Dhotre",
+    price: 200,
+  },
+  {
+    id: 4,
+    img: Book4,
+    title: "Software Engineering",
+    rating: 4.4,
+    author: "A.A. Puntambekar",
+    price: 320,
+  },
+  {
+    id: 5,
+    img: Book5,
+    title: "Computer Networking",
+    rating: 4.5,
+    author: "Abhishek Jaguessar",
+    price: 280,
+  },
+];
+
+const NextArrow = ({ onClick }) => (
+    <div
+      className="absolute top-1/2 transform -translate-y-1/2 right-2 text-black cursor-pointer z-10"
+      onClick={onClick}
+    >
+      <FaChevronRight size={30} />
+    </div>
+  );
+
+  const PrevArrow = ({ onClick }) => (
+    <div
+      className="absolute top-1/2 transform -translate-y-1/2 left-1 text-black cursor-pointer z-10"
+      onClick={onClick}
+    >
+      <FaChevronLeft size={30} />
+    </div>
+  );
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
 const AddBookForm = () => {
   const [formData, setFormData] = useState({
@@ -41,6 +141,7 @@ const AddBookForm = () => {
   };
 
   return (
+    <>
     <div className="max-w-2xl mx-auto mt-[100px] p-10 bg-gradient-to-br from-white to-blue-50 shadow-xl rounded-2xl">
       {/* ✅ Success Message */}
       {successMessage && (
@@ -125,7 +226,46 @@ const AddBookForm = () => {
           Submit Book
         </button>
       </form>
+
+      
     </div>
+
+    <div className='addedbook p-[40px]'> <h2 className='text-3xl'> Your Added Book </h2>
+    <Slider {...settings}>
+          {booksData.map(({ id, img, title, rating, author, price }) => (
+            <div key={id} className="p-10">
+              <div
+                className="space-y-4 p-3 bg-white rounded-xl shadow-md hover:shadow-lg hover:shadow-[#004aad] transition-shadow duration-300"
+                style={{ width: "300px" }} // Adjusted width for the book cover
+              >
+                <img
+                  src={img}
+                  alt={title}
+                  className="h-[240px] w-[185px] rounded-xl object-cover rounded-md mx-auto"
+                />
+                <div className="text-black text-center">
+                  <h3 className="font-semibold">{title}</h3>
+                  <p className="text-sm text-gray-700">{author}</p>
+                  <div className="flex items-center justify-center gap-1">
+                    <FaStar className="text-yellow-500" />
+                    <span>{rating}</span>
+                  </div>
+                  <div className="text-lg font-bold">Rs. {price}</div>
+                 
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+
+        {/* View All Books Button */}
+        <div className="flex justify-center mt-10">
+          <button className="text-center cursor-pointer bg-[#004aad] text-white py-2 px-6 rounded-md hover:bg-[#003a8c] transition-colors duration-300">
+            View All
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
